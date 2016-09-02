f8a1e5ae4986a669f75eb873b4ebc5915af6d04c
priya121 <priya121@github.com>
2016-09-02 09:42:17 +0100

    Adds row wins and board full/empty tests.

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,15 @@
 class Board
 
+  attr_reader :cells
+
   def initialize(cells)
     @cells = cells
+  end
+
+  def self.create_empty
+    Board.new(["-", "-", "-",
+               "-", "-", "-",
+               "-", "-", "-"])
   end
 
   def empty?
@@ -10,6 +18,25 @@ class Board
 
   def full?
     !@cells.include?("-")
+  end
+
+  def win_for?(mark)
+    winning_row(mark) 
+  end
+
+  def equals?(board_object)
+     board_object.cells == @cells
+  end
+
+  private
+
+  def rows  
+    (0..9 - 1).each_slice(3).to_a
+  end
+
+  def winning_row(mark)
+    rows.map {|combo| combo.all? {|position|
+      @cells[position] == mark}}.include? true
   end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,27 +1,100 @@
 require 'board' 
 describe Board do 
+
   EMPTY_BOARD = Board.new(["-", "-", "-",
                            "-", "-", "-",
                            "-", "-", "-"])
 
-  FULL_BOARD = Board.new(["X", "O", "X",
-                          "X", "O", "O",
-                          "O", "X", "X"])
+  X_FIRST_COLUMN_WIN = Board.new(["X", "O", "X",
+                                  "X", "O", "O",
+                                  "X", "X", "O"])
 
-  it 'determines if a board is empty' do 
-    expect(EMPTY_BOARD.empty?).to eq true
+  context 'empty board' do
+    it 'creates empty board' do
+      expect(Board.create_empty.equals?(EMPTY_BOARD)).to be true
+    end
+
+    it 'determines if a board is empty' do 
+      expect(EMPTY_BOARD.empty?).to eq true
+    end
+
+    it 'determines if a board is not empty' do
+      full_board = Board.new(["X", "O", "X",
+                              "X", "O", "O",
+                              "O", "X", "X"])
+      expect(full_board.empty?).to eq false 
+    end
+
+    it 'determines if a board is full' do 
+      full_board = Board.new(["X", "O", "X",
+                              "X", "O", "O",
+                              "O", "X", "X"])
+      expect(full_board.full?).to eq true 
+    end
+
+    it 'determines if a board is not full' do 
+      expect(EMPTY_BOARD.full?).to eq false 
+    end
   end
 
-  it 'determines if a board is not empty' do
-    expect(FULL_BOARD.empty?).to eq false 
-  end
+  context 'row wins' do
+    it 'determines a first row win for X' do
+      x_first_row_win = Board.new(["X", "X", "X",
+                                   "X", "O", "O",
+                                   "O", "X", "O"])
+      expect(x_first_row_win.win_for?("X")).to eq true 
+    end
 
-  it 'determines if a board is full' do 
-    expect(FULL_BOARD.full?).to eq true 
-  end
+    it 'determines no win for O when X wins' do
+      x_first_row_win = Board.new(["X", "X", "X",
+                                   "X", "O", "O",
+                                   "O", "X", "O"])
+      expect(x_first_row_win.win_for?("O")).to eq false 
+    end
 
-  it 'determines if a board is not full' do 
-    expect(EMPTY_BOARD.full?).to eq false 
+    it 'determines a first row win for O' do
+      o_first_row_win = Board.new(["O", "O", "O",
+                                   "O", "X", "X",
+                                   "X", "O", "X"])
+
+      expect(o_first_row_win.win_for?("O")).to eq true 
+    end
+
+    it 'determines no win for X when O wins' do
+      o_first_row_win = Board.new(["O", "O", "O",
+                                   "O", "X", "X",
+                                   "X", "O", "X"])
+
+      expect(o_first_row_win.win_for?("X")).to eq false 
+    end
+
+    it 'determines a second row win for X' do
+      x_second_row_win = Board.new(["X", "O", "O",
+                                    "X", "X", "X",
+                                    "O", "X", "O"])
+      expect(x_second_row_win.win_for?("X")).to eq true 
+    end
+
+    it 'determines a second row win for O' do
+      o_second_row_win = Board.new(["O", "X", "X",
+                                    "O", "O", "O",
+                                    "X", "O", "X"])
+      expect(o_second_row_win.win_for?("O")).to eq true 
+    end
+
+    it 'determines a third row win for X' do
+      x_third_row_win= Board.new(["X", "O", "O",
+                                   "O", "O", "X",
+                                   "X", "X", "X"])
+      expect(x_third_row_win.win_for?("X")).to eq true 
+    end
+
+    it 'determines a third row win for O' do
+      o_third_row_win = Board.new(["O", "X", "X",
+                                   "X", "X", "O",
+                                   "O", "O", "O"])
+      expect(o_third_row_win.win_for?("O")).to eq true 
+    end
   end
 
 end

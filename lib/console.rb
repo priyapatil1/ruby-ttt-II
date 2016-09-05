427153e9a2ebdbf693f2e6de28df65bca2541ec3
priya121 <priya121@github.com>
2016-09-05 14:23:20 +0100

    Refactors format board display.

--- lib/console.rb
+++ lib/console.rb
@@ -7,26 +7,16 @@ class Console
   end
 
   def format_board_display
-    rows = create_rows
     display = ROW_SEPARATOR
-    rows.each do |row|
-       display = draw_row(display, row)
-    end
-    display
+    rows = create_rows
+    display += "| " + rows.flat_map { |row|
+      [row, ROW_SEPARATOR]}.join(" | ")
   end
 
   private
 
   def create_rows
     @game.board.cells.each_slice(3)
-  end
-
-  def draw_row(display, row)
-    display += "| "
-    row.each do |cell|
-      display += cell.to_s + " | "
-    end
-    display << ROW_SEPARATOR
   end
 
 end

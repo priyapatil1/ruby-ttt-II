fc20f911c4dd28672c7e0e623155dce23063d22c
priya121 <priya121@github.com>
2016-09-05 10:45:26 +0100

    Adds empty board check.

--- lib/board.rb
+++ lib/board.rb
@@ -1,55 +1,11 @@
 class Board
 
-  attr_reader :cells
-
   def initialize(cells)
     @cells = cells
   end
 
-  def self.empty_board
-    Board.new(["-", "-", "-",
-               "-", "-", "-",
-               "-", "-", "-"])
-  end
-
-  def self.with_moves(cells)
-    Board.new(cells)
-  end
-
   def empty?
-    !@cells.include?("O") && !@cells.include?("X")
-  end
-
-  def full?
-    !@cells.include?("-")
-  end
-
-  def win_for?(mark)
-    winning_row(mark) || winning_column(mark)
-  end
-
-  def equals?(board_object)
-     board_object.cells == @cells
-  end
-
-  private
-
-  def rows  
-    (0..9 - 1).each_slice(3).to_a
-  end
-
-  def columns
-    rows.transpose
-  end
-
-  def winning_row(mark)
-    rows.map {|row| row.all? {|position|
-      @cells[position] == mark}}.include? true
-  end
-
-  def winning_column(mark)
-    columns.map {|column| column.all? {|position|
-      @cells[position] == mark}}.include? true
+    @cells == (1..9).to_a
   end
 
 end

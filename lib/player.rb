b0425de064ff23d855dbff8948453b891774b9c4
priya121 <priya121@github.com>
2016-09-06 17:15:36 +0100

    Adds ability to set another player move

--- lib/player.rb
+++ lib/player.rb
@@ -1,17 +1,18 @@
 class Player
 
   attr_reader :mark
+  attr_reader :move_given
 
   def initialize(mark)
     @mark = mark 
   end
 
   def set_current_move(move_given)
-    move_given
+    @move_given = move_given
   end
 
   def current_move
-    2
+    @move_given
   end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -28,6 +28,12 @@ describe Player do
       player_x.set_current_move(2)
       expect(player_x.current_move).to eq 2
     end
+
+    it 'exposes another current move' do
+      player_x = Player.new("X")
+      player_x.set_current_move(5)
+      expect(player_x.current_move).to eq 5
+    end
   end
 
 end

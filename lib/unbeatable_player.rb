f7b0f5139991ee7448c056f0dd50b8c4ada2f5e3
priya121 <priya121@github.com>
2016-09-14 19:39:55 +0100

    Removes renamed spec and adds Unbeatable player mark test

--- lib/unbeatable_player.rb
+++ lib/unbeatable_player.rb
@@ -1,5 +1,11 @@
 class UnbeatablePlayer 
 
+  attr_reader :mark
+
+  def initialize(mark)
+    @mark = "X"
+  end
+
   def move(board)
     7
   end
--- spec/unbeatable_player_spec.rb
+++ spec/unbeatable_player_spec.rb
@@ -2,12 +2,16 @@ require 'board'
 require 'unbeatable_player'
 
 describe UnbeatablePlayer do
+  it 'has a mark' do
+    player = UnbeatablePlayer.new("X")
+    expect(player.mark).to eq "X"
+  end
 
   it 'plays the winning move given two options' do
+    unbeatable_player = UnbeatablePlayer.new("X")
     board = Board.with_moves(["X", "X", "X",
                               "X", "O",  "O",
                                7,   8,  "O"])
-    unbeatable_player = UnbeatablePlayer.new
     expect(unbeatable_player.move(board)).to eq 7
   end
 
